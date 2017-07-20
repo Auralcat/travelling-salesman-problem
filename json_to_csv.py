cf5d709251729469d63e1f808cbb379feb0ea578
Auralcat <ashleyzt@hi2.in>
2017-07-20 18:00:41 -0300

    Sketched the initial methods for json_to_csv.py

--- json_to_csv.py
+++ json_to_csv.py
@@ -8,8 +8,19 @@ import csv
 import os
 
 def getJSONData(json_filename):
+    """Gets the data from the JSON file."""
     json_filepath = os.path.join(os.getcwd(), json_filename)
     with open(json_filepath, 'r') as inputfile:
+        content = inputfile.readlines()
 
-def convertToCSV():
+    return content
+
+def convertToCSV(json_data):
     """Converts the JSON data into a CSV file."""
+
+    json_parsed = json.loads(json_data)
+    csvfilename = 'coords.csv'
+    with open(csvfilename, 'w') as csvfile:
+        csvwriter = csv.writer(csvfile)
+
+    print("File %s saved." % csvfilename)
